e066d99c286a0051fc8e452f2284cfd5554c5f99
Chiara Caprasi <caprasichiara@gmail.com>
2022-02-03 20:10:48 +1100

    Recreate file for task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-/**
- * main - prints the phrase as below
- * return - 0 if succesful 
- */
+/*
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if success, non-zero otherwise
+*/
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
