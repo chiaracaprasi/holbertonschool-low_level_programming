afa2c393da103cf62418c69b2c84535a704122c1
Chiara Caprasi <caprasichiara@gmail.com>
2022-02-03 19:31:12 +1100

    Refactor file 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-/*  Main function to print text 
- * Return always 0
+/**
+ * main - prints out the phrase as below
+ * return - always 0 if successful 
  */
-
 int main(void)
-{   
+{
+  
    puts("\"Programming is like building a multilingual puzzle");
    return(0);
+   
  }
