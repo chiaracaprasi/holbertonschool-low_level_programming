42931df0453e6619d5cfe60f9db46c2bdec8ffec
Chiara Caprasi <caprasichiara@gmail.com>
2022-02-04 13:42:39 +1100

    Add 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lower case
+ * followed by new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a'; 
+
+	while (ch  <= 'z')
+	{
+		if (chr!= 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++; 
+	}
+	putchar('\n');
+	return (0);
+}
