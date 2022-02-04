d38b58bc6edb953e06ec2f9f9fb494150ebfa588
Chiara Caprasi <caprasichiara@gmail.com>
2022-02-04 14:04:46 +1100

    Replace '' with  in 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
@@ -13,6 +11,6 @@ int main(void)
 
 	for (n = 0; n < 10; n++)
 		printf("%d", n);
-	printf('\n');
+	printf("\n");
 	return (0);
 }
