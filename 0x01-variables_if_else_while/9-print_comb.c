3fdc5938c74d5bffdfd526f3e0c2bd3e50b5e885
Chiara Caprasi <caprasichiara@gmail.com>
2022-02-04 16:46:17 +1100

    Add 9-print_comb.c file and script

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single digit numbers
+ * followed by new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
