21f0acd42b11adcb72f65aa2c5b9f14d475f918f
Chiara Caprasi <caprasichiara@gmail.com>
2022-02-06 13:16:26 +1100

    Add 102-print_comb5.c file and script

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two digit numbers
+ * followed by new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, n2;
+
+	for (n = 0; n <= 99; n++)
+	{
+		for (n2 = 0; n2 <= 99; n2++)
+		{
+			if (n2 < n)
+			{
+				putchar((n / 10) + '0');
+				putchar((n % 10) + '0');
+				putchar(' ');
+				putchar((n2 / 10) + '0'); 
+				putchar((n2 % 10) + '0');
+				if (n != 98 || n2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
