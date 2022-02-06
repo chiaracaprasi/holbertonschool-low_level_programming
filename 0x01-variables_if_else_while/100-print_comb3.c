6694585d2b82cea00fc1ee5b6a6ea110ce9201be
Chiara Caprasi <caprasichiara@gmail.com>
2022-02-06 12:34:18 +1100

    Add file and script for 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single digit numbers
+ * followed by new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, n2;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (n2 = '0'; n2 <= '9'; n2++)
+		{
+			if (n2 > n)
+			{
+				putchar(n);
+				putchar(n2);
+				if (n != '9' || n2 != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+}
