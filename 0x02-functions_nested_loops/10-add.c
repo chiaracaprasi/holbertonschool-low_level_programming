5e9adc4282ce9d239f9a0340f29a89c23d60268f
Chiara Caprasi <caprasichiara@gmail.com>
2022-02-08 01:06:16 -0800

    Add 10-add.c file and script

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *add- a function that add two integers and
+ *@n: intergers to be added
+ *@n2: intergers to be added
+* Return: the result of the sum;
+ */
+int add(int n, int n2)
+{
+	int sum;
+
+	sum = n + n2;
+
+	return (sum);
+}
