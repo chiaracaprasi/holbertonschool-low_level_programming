e6d7191a039654444c8acbf3d700b9821e7ced51
Chiara Caprasi <caprasichiara@gmail.com>
2022-02-08 11:36:47 +1100

    Add 0-putchar.c in the right file format for Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar
+ * followed by new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar(95);
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
