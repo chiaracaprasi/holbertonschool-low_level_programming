0d8392ab3a59f038027c5772714f567794a66017
Chiara Caprasi <caprasichiara@gmail.com>
2022-02-07 21:24:12 -0800

    Add 7-print_last_digit file and script

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *print_last_digit- computes absolute value of a number
+ * @n: number to check
+ * Return: n;
+ */
+int print_last_digit(int n)
+{
+	_putchar(n % 10);
+	return (n);
+
+}
