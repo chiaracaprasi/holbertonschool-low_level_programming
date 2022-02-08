08b46020ace8069e9de9dfcdeb06cbb142dc2856
Chiara Caprasi <caprasichiara@gmail.com>
2022-02-08 14:25:44 +1100

    Add file 2-print_alphabet_x10.c for Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet in lower care 10 times
+ * followed by new line
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int t;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		for (t = 0; t <= 10; t++)
+		{
+		_putchar(ch);
+		}
+	}
+	_putchar('\n');
+}
