79364e61a4918d04eab4da7f23ed14379d824b7a
Chiara Caprasi <caprasichiara@gmail.com>
2022-02-09 23:19:21 -0800

    Update filr 5-more..

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,9 +10,15 @@ void more_numbers(void)
 	int times;
 
 	for (times = 0; times <= 9; times++)
-        {
-                for (num = 0; num <= 14; num++)
-                _putchar(num);
-                _putchar('\n');
-        }
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+			{
+				_putchar(num / 10 + '0');
+			}
+			_putchar(num % 10 + '0');
+		}
+		_putchar('\n');
+	}
 }
