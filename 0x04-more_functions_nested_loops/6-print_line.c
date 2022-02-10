d01888c65e34b5e3a229c56fefd6662d1bb33298
Chiara Caprasi <caprasichiara@gmail.com>
2022-02-09 23:43:49 -0800

    Fix script in 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,17 +7,18 @@
  */
 void print_line(int n)
 {
-	int times;
-
 	if (n <= 0)
-	  _putchar('\n');
-
+	{
+		_putchar('\n');
+	}
 	else
-	  {
-	    for (times = '0'; times <= n; times++)
-	      {
-		_putchar(n);
-	      }
-	  }	
+	{
+		int times;
+
+		for (times = 1; times <= n; times++)
+		{
+			_putchar('_');
+		}
+	}
 	_putchar('\n');
 }
