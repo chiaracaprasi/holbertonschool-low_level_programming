63eb8df30e2551eda369b8543bae21a94956c797
Chiara Caprasi <caprasichiara@gmail.com>
2022-02-13 16:17:26 -0800

    Add var in comments in file 0-reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
-/**
+1;95;0c/**
  * reset_to_98 - takes pointer as parameter and change the value to 98
- *
+ * @n pointer to number given as parameter 
  * Return: Always 0.
  */
 void reset_to_98(int *n)
