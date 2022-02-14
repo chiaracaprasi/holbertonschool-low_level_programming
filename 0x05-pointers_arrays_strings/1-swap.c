1d8498432b10074beeac95132c0fdfdc7c07e66b
Chiara Caprasi <caprasichiara@gmail.com>
2022-02-13 16:28:35 -0800

    Fix file name

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - swaps values of two int
+ * @a: pointer to number given as parameter
+ * @b: pointer to number given as parameter
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+
+}
