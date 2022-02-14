0ff5477035b9454b2ab6660fcfe6628e7b0e1db5
Chiara Caprasi <caprasichiara@gmail.com>
2022-02-14 02:16:20 -0800

    Add 7-puts_half file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - returns value of a string
+ * @s: pointer to string given as parameter
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int n;
+
+	for (n = 0; *s != '\0'; s++)
+		n++;
+
+	return (n);
+
+}
+
+/**
+  * _puts - print a string followed by new line
+ * @str: pointer to string given as parameter
+ * Return: Always 0.
+ */
+
+void _puts(char *str)
+{
+	int i;
+	int len = _strlen(str);
+
+	for (i = 0; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+
+}
+
+/**
+ * puts_half - returns value of a string
+ * @str: pointer to string given as parameter
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+	char *b, *e;
+
+	b = str;
+	e = &str[_strlen(str) - 1];
+
+	while (e != b)
+	{
+
+		_puts(str);
+	}
+}
