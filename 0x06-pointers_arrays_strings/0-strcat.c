89f2107d9db1048105b6cbf674fbecbb541e8f70
Chiara Caprasi <caprasichiara@gmail.com>
2022-02-15 19:02:22 -0800

    Add scripf for 0-strcat.c for task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: parameter char given
+ * @src: parameter char given
+ *
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 1;
+	int j = 0;
+
+	while
+		(dest[++i]);
+	while
+		(dest[i++] = src[j++]);
+
+	return (dest);
+}
