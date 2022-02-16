767f0bfdd41ed7a4069fab4eafede73826778f02
Chiara Caprasi <caprasichiara@gmail.com>
2022-02-15 19:45:12 -0800

    Add file for task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * _strlen - returns value of a string
+ * @s: pointer to string given as parameter
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int n;
+
+	for (n = 0; *s != '\0'; s++)
+		n++;
+
+	return (n);
+
+}
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: parameter char given
+ * @src: parameter char given
+ * @n: number given as parameter
+ * Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = _strlen(dest);
+	int i;
+
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+}
