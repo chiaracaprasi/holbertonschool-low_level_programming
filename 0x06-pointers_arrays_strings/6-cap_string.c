aeff5ea5559cd1386c87995d9ad866473ce59d81
Chiara Caprasi <caprasichiara@gmail.com>
2022-02-16 21:19:30 -0800

    Refactor code in 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,26 +2,6 @@
 #include <stdio.h>
 
 /**
- * string_toupper - changes all lowercase letters to uppercase
- * @s: string passed
- *
- * Return: string.
- */
-char *string_toupper(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		if (s[i] >= 'a' && s[i] <= 'z')
-			s[i] = s[i] - 32;
-		i++;
-	}
-
-	return (s);
-}
-
-/**
  * cap_string -  capitalizes all words of a string.
  * @str: string passed
  *
