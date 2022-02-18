642bfef236a91f1b73ff95eb0245d8504cf2d00c
Chiara Caprasi <caprasichiara@gmail.com>
2022-02-18 00:54:49 -0800

    Delete extra code

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,36 +2,23 @@
 #include <stdio.h>
 
 /**
- * print_numbers - print an integer
+ * print_number - print an integer
  * @n: number given as parameter
  *
  * Return: string.
  */
 void print_number(int n)
- {
-	 if (n < 0)
-	 {
-		 _putchar('-');
-		 n = -n;
-	 }
-	 if (n == 0)
-		 _putchar('0');
-	 
-	 if (n / 10)
-	 {
-		 print_number(n / 10);
-	 }
-	 _putchar((n % 10) + '0');
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
 {
-	print_number(1062);
-	_putchar('\n');
-	return (0);
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n == 0)
+		_putchar('0');
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
 }
