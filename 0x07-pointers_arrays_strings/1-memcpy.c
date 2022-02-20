9618cdabe96c04b149472925c414e503c8776a27
Chiara Caprasi <caprasichiara@gmail.com>
2022-02-20 15:25:18 -0800

    Add file for task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy -copies memory area.
+ * @src: memory area to be copied from
+ * @n: the size of the memory to print
+ * @dest: memory area to copy to
+ *
+ * Return: pointer to the memory area destination
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+	unsigned int count = 0;
+
+	while (count < n)
+	{
+		*(dest + count) = *(src + count);
+		count++;
+	}
+
+	return (dest);
+}
