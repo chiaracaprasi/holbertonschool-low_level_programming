35beac233a55f68ed4165c071eb67e913983d31a
Chiara Caprasi <caprasichiara@gmail.com>
2022-02-21 04:31:24 -0800

    Fix betty style

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -33,7 +33,7 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int len = 0;
 
-	while( *s != '\0' && _strchr(accept , *s) != 0)
+	while (*s != '\0' && _strchr(accept, *s) != 0)
 	{
 		s++;
 		len++;
