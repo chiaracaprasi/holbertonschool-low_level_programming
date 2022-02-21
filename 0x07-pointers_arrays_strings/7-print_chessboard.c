1547d4cf54bfed99230fdb180a1f9d40498535e0
Chiara Caprasi <caprasichiara@gmail.com>
2022-02-21 04:42:00 -0800

    Add file for task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard.
+ * @a: array given
+ *
+ * Return:0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int row, column;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (column = 0; column < 8; column++)
+		{
+			_putchar(a[row][column]);
+		}
+			_putchar('\n');
+	}
+}
