1a0d8ba1a19c10a0310ab6837848a3cfa8e2d4e3
Chiara Caprasi <caprasichiara@gmail.com>
2022-02-20 22:20:53 -0800

    Add file for task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strbrk - searches a string for any of a set of bytes.
+ * @s: string given
+ * @accept: bytes to locate in string s
+ *
+ * Return: pointer to the first occurrence of the character c in the string s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int count1 = 0;
+	int count2;
+
+	while (s[count1] != '\0')
+	{
+		count2 = 0;
+		while (accept[count2] != '\0')
+		{
+			if (s[count1] == accept[count2])
+				return (s + count1);
+			count2++;
+		}
+		count1++;
+	}
+	return ('\0');
+}
