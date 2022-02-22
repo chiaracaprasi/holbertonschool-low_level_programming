a396c7e945e9458449150f62818593ac9b994cce
Chiara Caprasi <caprasichiara@gmail.com>
2022-02-22 01:53:38 -0800

    Add file for task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets value of a pointer to char
+ * @to: pointer
+ * @s: pointer to pointer
+ * Return:0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
