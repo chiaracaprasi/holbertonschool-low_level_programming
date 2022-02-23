3d1df10f75d73c5bef20168795904b8bd01c4800
Chiara Caprasi <caprasichiara@gmail.com>
2022-02-23 01:29:15 -0800

    Add file for task 7

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * prime_test - tells if input integer is a prime number by returning 1
+ * @n: the given number to check
+ * @i: incrementing number to test against
+ * Return: 1 if the input integer is a prime number, otherwise return 0.
+ */
+int palindrome_found(char *s, int i, int len)
+{
+	if (*(s + i) != *( s + len - 1))
+	{
+		return (0);
+	}
+	if (i >= len)
+		return (1);
+	else
+		return (palindrome_found(s, i + 1, len - 1));
+}
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string given
+ * Return: length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+
+}
+
+/**
+ * is_palindrome - checks if given string is a palindrome
+ * @s: given string
+ *
+ * Return: 1 if string is palindrome, otherwise return 0.
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	else
+		return(palindrome_found(s, 0, _strlen_recursion(s)));
+}
