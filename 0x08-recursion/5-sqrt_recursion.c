558f69cc2ff08ad3065dfcbbe6b4a3468c8d8284
Chiara Caprasi <caprasichiara@gmail.com>
2022-02-22 21:27:17 -0800

    Update function for task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,26 @@
 #include "main.h"
 #include <stdio.h>
+
+/**
+ * natural_square_root- returns natural square root of a number
+ * @y: given number
+ * @n: given number
+ * Return: returns natural square root of a number given
+ */
+int natural_square_root(int y, int x)
+{
+	if (y > (x / 2))
+	{
+		return (-1);
+	}
+	if (y * y == x)
+	{
+		return (y);
+	}
+	else
+		return (natural_square_root(y + 1, x));
+}
+
 /**
  * _sqrt_recursion- returns natural square root of a number
  * @n: given number
@@ -9,13 +30,12 @@
 
 int _sqrt_recursion(int n)
 {
-//	if (n == 0)
-//		return (1);
-
 	if (n < 0)
+	{
 		return (-1);
+	}
+	else if (n == 1)
+		return (1);
 
-	else
-
-
+	return (natural_square_root(1, n));
 }
