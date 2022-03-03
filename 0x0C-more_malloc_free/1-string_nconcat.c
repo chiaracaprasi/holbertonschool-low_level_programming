cfa7d115987f9aeb5581b0ad045efbaea244a3a7
Chiara Caprasi <caprasichiara@gmail.com>
2022-03-02 19:26:06 -0800

    fix: add space in memory for \0

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -86,7 +86,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= len2)
 		n = len2;
 
-	result = malloc((len1 + n) * sizeof(char));
+	result = malloc((len1 + n + 1) * sizeof(char));
 
 	if (result == NULL)
 		return (NULL);
