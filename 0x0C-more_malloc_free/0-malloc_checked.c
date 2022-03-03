9ef5c293d439d178c0d4629b25441fed01fe09cb
Chiara Caprasi <caprasichiara@gmail.com>
2022-03-02 17:16:40 -0800

    fix: checker issue

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -11,14 +11,12 @@
 
 void *malloc_checked(unsigned int b)
 {
-	unsigned int *ptr;
+	void *ptr;
 
-	if (b == 0)
-		exit(98);
-
-	ptr = malloc(sizeof(int) * b);
+	ptr = malloc(b);
 	if (ptr == NULL)
+	{
 		exit(98);
-
+	}
 	return (ptr);
 }
