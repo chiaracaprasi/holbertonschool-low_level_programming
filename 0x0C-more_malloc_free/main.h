12893326c06ef646713e6d54ae89756138ac7cee
Chiara Caprasi <caprasichiara@gmail.com>
2022-03-02 18:45:16 -0800

    build: add function prototype to main

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,6 +8,6 @@ int *array_range(int min, int max);
 int _putchar(char c);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
-
+char *_strncat(char *dest, char *src, int n);
 
 #endif /* MAIN_H */
