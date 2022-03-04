0028012aee6a2914e164edcede74a0c1f09c0ef3
Chiara Caprasi <caprasichiara@gmail.com>
2022-03-03 17:17:58 -0800

    build: add function prototype for task 4

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,5 +10,6 @@ int _putchar(char c);
 char *_strcpy(char *dest, char *src);
 unsigned int _strlen(char *s);
 char *_strncat(char *dest, char *src, unsigned int n);
+char *_memcpy(void *dest, void *src, unsigned int n);
 
 #endif /* MAIN_H */
