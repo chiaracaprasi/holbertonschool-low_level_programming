5167cdd17687da9c13131acf84cbfc66bede1244
Chiara Caprasi <caprasichiara@gmail.com>
2022-03-09 01:21:08 -0800

    fix: berry style

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
-#include "3-calc.c"
+#include "3-calc.h"
 
 /**
  * get_op_func - selects correct function to perform operation asked by user.
  *
  * @s: operator passed as argument to the program
- * Return - null or index 
+ *
+ *Return: null or index
  */
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
-    i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
 
-    while (ops[i].op != NULL)
-    {
-	    if (*ops[i].op == *s)
-		    return (ops[i].f);
-	    i++;
-    }
-    return (NULL);
+	i = 0;
+	while (ops[i].op != NULL)
+	{
+		if (*ops[i].op == *s)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
