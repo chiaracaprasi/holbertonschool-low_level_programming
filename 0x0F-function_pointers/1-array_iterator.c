d229abe216093616f099b3be793fc6a876e11f90
Chiara Caprasi <caprasichiara@gmail.com>
2022-03-08 16:25:35 -0800

    build: add file for task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator -executes function given as parameter on each array element
+ * @array: name of array passed
+ * @size: size of array
+ * @action: pointer to function 
+ *
+ * Return: Nothing.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int count = 0;
+
+	if (array != NULL || action!= NULL)
+	{
+		while (count < size)
+		{
+			count++;
+			action(array[count]);
+		}
+	}
+}
