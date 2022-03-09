9b44ecc71e4d0b7ce86bcdc77718039a434be3fe
Chiara Caprasi <caprasichiara@gmail.com>
2022-03-08 22:01:25 -0800

    build: fist commit for file for task 3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "3-calc.c"
+
+/**
+ * get_op_func - selects correct function to perform operation asked by user.
+ *
+ * @s: operator passed as argument to the program
+ * Return - null or index 
+ */
+int (*get_op_func(char *s))(int, int)
+{
+    op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i;
+    i = 0;
+
+    while (ops[i].op != NULL)
+    {
+	    if (*ops[i].op == *s)
+		    return (ops[i].f);
+	    i++;
+    }
+    return (NULL);
+}
