0da0942bb9b2764dc8fa48290c3e6c1a20336e23
Chiara Caprasi <caprasichiara@gmail.com>
2022-03-09 01:41:21 -0800

    fix: add condition for error for division by 0 case

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,6 +14,7 @@ int main(int argc, char *argv[])
 {
 	int (*f)(int, int);
 	int arg1, arg2;
+	char divide;
 
 	if (argc != 4)
 	{
@@ -28,13 +29,21 @@ int main(int argc, char *argv[])
 	}
 
 	f = *get_op_func(argv[2]);
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[3]);
 
 	if (f == NULL)
 	{
 		printf("Error\n");
 		exit(99);
+	}
+
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+	divide = *argv[2];
+
+	if ((divide == '/' || divide == '%') && (arg2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
 	}
 
 	printf("%d\n", f(arg1, arg2));
