04057a7fa8e54dab15639034de99bd2c6138e81f
Chiara Caprasi <caprasichiara@gmail.com>
2022-03-08 17:17:02 -0800

    fix: add contion for null in line with betty

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,7 +5,7 @@
  * array_iterator -executes function given as parameter on each array element
  * @array: name of array passed
  * @size: size of array
- * @action: pointer to function 
+ * @action: pointer to function
  *
  * Return: Nothing.
  */
@@ -14,12 +14,12 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int count = 0;
 
-	if (array != NULL || action!= NULL)
+	if (array == NULL || action == NULL)
+		return;
+
+	while (count < size)
 	{
-		while (count < size)
-		{
-			count++;
-			action(array[count]);
-		}
+		count++;
+		action(array[count]);
 	}
 }
