5d032c2d161e9631c4f173e60b1cce1b0145e0dd
Chiara Caprasi <caprasichiara@gmail.com>
2022-03-16 18:56:02 -0700

    fix: add another edge case

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,13 +14,20 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *newNode = malloc(sizeof(list_t));
 	char *str_cpy = strdup(str);
 
-	if (newNode == NULL || str_cpy == NULL)
+	if (newNode == NULL)
+		return (NULL);
+
+	if (str_cpy == NULL)
 		return (NULL);
 
 	newNode->str = str_cpy;
 	newNode->len = strlen(str_cpy);
-	if (*head != NULL)
+
+	if (*head == NULL)
+		newNode->next = NULL;
+	else
 		newNode->next = *head;
+
 	*head = newNode;
 	return (newNode);
 }
