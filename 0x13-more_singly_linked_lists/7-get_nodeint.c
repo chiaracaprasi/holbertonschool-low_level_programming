6de8afbc1e2a5f9e615d037577584db660b674ff
Chiara Caprasi <caprasichiara@gmail.com>
2022-03-22 12:11:13 +1100

    fix: resolve logic and remove uncessary var

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,6 +4,25 @@
 #include "lists.h"
 
 /**
+ * listint_len -  returns number of elements in listint
+ * @h: linked list given
+ * Return: number of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	unsigned int nodes = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		nodes++;
+	}
+
+	return (nodes);
+}
+
+/**
  * get_nodeint_at_index - function that returns nth node of linked list.
  * @head: linked list given
  * @index: index of the node starting at 0
@@ -13,7 +32,7 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
-	listint_t *current = head;
+	unsigned int len = listint_len(head);
 
 	if (head == NULL)
 		return (0);
@@ -21,10 +40,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (index == 0)
 		return (head);
 
+	if (index > len)
+		return (NULL);
+
 	while (head != NULL && count < index)
 	{
 		count++;
 		head = head->next;
 	}
-	return (current);
+	return (head);
 }
