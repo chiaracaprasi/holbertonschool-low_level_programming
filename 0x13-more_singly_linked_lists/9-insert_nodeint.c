ea4c246520252af9e2af3270c593d278b5aee7ac
Chiara Caprasi <caprasichiara@gmail.com>
2022-03-23 13:34:47 +1100

    fix: edge cases

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -54,22 +54,26 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 
 	tmp = *head;
-	if (head == NULL && idx == 0)
+	if (idx == 0)
 	{
 		new->next = tmp;
 		*head = new;
 		return (new);
 	}
-
-	for (count = 0; count < idx - 1; count++)
+	else
 	{
-		if (tmp == NULL || tmp->next == NULL)
-			return (NULL);
+		for (count = 0; count < idx - 1; count++)
+		{
+			if (tmp == NULL || tmp->next == NULL)
+			{
+				free(new);
+				return (NULL);
 
-		tmp = tmp->next;
+			}
+			tmp = tmp->next;
+		}
+		new->next = tmp->next;
+		tmp->next = new;
 	}
-	new->next = tmp->next;
-	tmp->next = new;
-
 	return (new);
 }
