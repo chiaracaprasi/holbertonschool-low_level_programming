5e5aa098bd346ea1d48df25d776a49dda8dda1dc
Chiara Caprasi <caprasichiara@gmail.com>
2022-03-28 15:52:23 +1100

    fix: add edge case for write failure

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, sz_write;
 	int len = strlen(text_content);
 
 	if (filename == NULL)
@@ -30,7 +30,10 @@ int create_file(const char *filename, char *text_content)
 	if (fd < 0)
 		return (-1);
 
-	write(fd, text_content, len);
+	sz_write = write(fd, text_content, len);
+
+	if (sz_write < 0)
+		return (-1);
 
 	close(fd);
 	return (1);
