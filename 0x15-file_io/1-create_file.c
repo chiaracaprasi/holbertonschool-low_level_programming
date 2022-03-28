cd11404116717731d33f07e76adaae00d15bbcaa
Chiara Caprasi <caprasichiara@gmail.com>
2022-03-28 15:49:19 +1100

    build: add file for task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "main.h"
+
+/**
+ * create_file - a function that creates a file.
+ * @filename: name of file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success or -1 on failure
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int len = strlen(text_content);
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (fd < 0)
+		return (-1);
+
+	write(fd, text_content, len);
+
+	close(fd);
+	return (1);
+}
