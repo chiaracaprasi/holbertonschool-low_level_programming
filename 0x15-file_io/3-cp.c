25da662056564cab4c0baa59eb5272e105a87ca7
Chiara Caprasi <caprasichiara@gmail.com>
2022-03-28 19:47:18 +1100

    fix: add edge cases for task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,7 +16,7 @@
 
 int main(int argc, char *argv[])
 {
-	int fd1;
+	int fd1, fd2, closef1, closef2;
 
 	if (argc != 3)
 	{
@@ -32,7 +32,28 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
+	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
-	close(fd1);
+	if (fd2 < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(fd1);
+		exit(99);
+	}
+
+	closef1 = close(fd1);
+	closef2 = close(fd2);
+
+	if (closef1 < 0 || closef2 < 0)
+	{
+		if (closef1 < 0)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+
+		if (closef2 < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+
+		exit(100);
+	}
+
 	return (0);
 }
