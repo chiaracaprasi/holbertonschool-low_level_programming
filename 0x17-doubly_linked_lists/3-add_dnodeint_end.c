c061640533bd983a0a6ce2b7f87f18c4af853003
Chiara Caprasi <caprasichiara@gmail.com>
2022-05-04 19:40:36 -0700

    fix: checker issue

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -28,8 +28,9 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	/* if empty list maake new node the head node */
 	if (*head == NULL)
 	{
-		newNode->prev = NULL;
 		*head = newNode;
+		newNode->prev = NULL;
+
 	}
 	else
 	{
